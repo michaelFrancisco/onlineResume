49df13cb6a4e0f5f12e428b567833cb1ac6095c9
Michael Francisco <michaelfrancisco64@gmail.com>
2020-09-09 06:42:02 +0800

    Updates

---
+++ precache-manifest.2155e6b644a318c29bed94b72a26c0b9.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22c65ea4c61a8b0dc84d74b3a8491f8c",
+    "url": "/onlineResume/index.html"
+  },
+  {
+    "revision": "ce1b150c8d0ee26b650a",
+    "url": "/onlineResume/static/css/2.eb096641.chunk.css"
+  },
+  {
+    "revision": "ce1b150c8d0ee26b650a",
+    "url": "/onlineResume/static/js/2.8dc37aee.chunk.js"
+  },
+  {
+    "revision": "6ab22d0762689750f706d1e715ad70f9",
+    "url": "/onlineResume/static/js/2.8dc37aee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "992c9cfb37b6bedff717",
+    "url": "/onlineResume/static/js/main.0a8fafdc.chunk.js"
+  },
+  {
+    "revision": "c7628a2f0327a8578d47",
+    "url": "/onlineResume/static/js/runtime-main.858c5d49.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/onlineResume/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/onlineResume/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/onlineResume/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/onlineResume/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/onlineResume/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/onlineResume/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/onlineResume/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/onlineResume/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/onlineResume/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/onlineResume/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/onlineResume/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/onlineResume/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/onlineResume/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/onlineResume/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/onlineResume/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/onlineResume/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/michaelfrancisco.github.io/onlineResume/precache-manifest.93bd9a07c8b1162d7a5c18ac43326e6f.js"
+  "/onlineResume/precache-manifest.2155e6b644a318c29bed94b72a26c0b9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/michaelfrancisco.github.io/onlineResume/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/onlineResume/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
