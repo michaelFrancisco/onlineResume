83084a7b94eecdfa07ddfc1bd7247402b47b84e2
Michael Francisco <michaelfrancisco64@gmail.com>
2020-09-11 10:12:01 +0800

    Updated Links

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
-import * as serviceWorker from './serviceWorker';
 import Resume from 'react-awesome-resume'
 
 const myJSONResume = require('./person.json')
@@ -12,4 +11,3 @@ ReactDOM.render(
   <Resume jsonResume={myJSONResume} theme={"default"} inline={false} />,
   document.getElementById("root")
 );
-serviceWorker.unregister();
